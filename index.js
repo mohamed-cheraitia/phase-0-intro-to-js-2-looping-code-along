3b0911fac8edbbe70e34a894b7c65f6ff89eeba4
moh1med <mohamedch047@gmail.com>
2023-08-26 14:23:55 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let msg = [];
+const names = ["sami", "karim", "Mohamed"];
+const event = "birthday";
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    msg.push(message);
+  }
+  return msg;
+}
+
+function countDown (i){
+
+while (i >= 0) {
+  console.log(i);
+   i--;
+}
+}
+
+countDown(10);
